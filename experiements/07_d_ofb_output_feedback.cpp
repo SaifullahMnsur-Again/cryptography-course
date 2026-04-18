e1eecb7c95da82ffbc8a59ad631dfa57a52d4645
Sayfullah Monsur Sujan Sarkar <210016429+SaifullahMnsur-Again@users.noreply.github.com>
2026-04-19 00:10:57 +0600

    Add OFB output feedback

---
+++ experiements/07_d_ofb_output_feedback.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+// #include <string>
+#include <iomanip> // for hexa decimal printing
+
+using namespace std;
+
+const int BLOCK_SIZE = 8; // 8 for DES, 16 for AES
+
+string xor_block_op(string block, string key) {
+    string xor_block = "";
+    for(int i = 0; i < BLOCK_SIZE; i++) {
+        xor_block += (block[i] ^ key[i]);
+    }
+    return xor_block;
+}
+
+string encryptSingleBlock(string block, string key) {
+    return xor_block_op(block, key);
+}
+
+string processOFB(string input_text, string key, string nonce) {
+    string output_text = "";
+    string curr_input = nonce;
+
+    for(int i = 0; i < input_text.length(); i += BLOCK_SIZE) {
+
+        int u_bytes = min(BLOCK_SIZE, (int)input_text.length() - i);
+        string plaintext_block = input_text.substr(i, u_bytes);
+
+        string O_j= encryptSingleBlock(curr_input, key);
+
+        string msb_u = O_j.substr(0, u_bytes);
+
+        string result_block = "";
+        for(int j = 0; j < u_bytes; j++) {
+            result_block += (plaintext_block[j] ^ msb_u[j]);
+        }
+
+        output_text += result_block;
+
+        curr_input = O_j;
+    }
+    return output_text;
+}
+
+
+void printHex(string data) {
+    for(int i = 0; i < data.length(); i++) {
+        if(i > 0 && i % BLOCK_SIZE == 0)
+            cout << "| ";
+        // hex = hexadecimal format
+        // setw(2) = word width 2
+        // setfill('0') = if generated word is less than 2 then fill with 0s from left
+        // (int)(unsigned char)data[i] = convert the charecter to unsigned charecter and then the integer ascii
+        // it prints the hexa decimal value of the charect
+        cout << hex << setw(2) << setfill('0') << (int)(unsigned char)data[i] << " ";
+    }
+    cout << dec << endl;
+}
+
+int main() {
+    cout << "--- OFB Mode Operation ---" << endl << endl;
+
+    string plaintext = "ADMINISTRATOR   ADMINISTRATOR";
+    string key = "ABCDEFGH"; // block size 8 byte = 64 bit
+    string nonce = "12345678";
+
+    cout << "Plaintext: " << plaintext << endl;
+    cout << "Key: " << key << endl;
+    cout << "Nonce: " << nonce << endl;
+
+    string ciphertext = processOFB(plaintext, key, nonce);
+    cout << "Cipher Text: " << ciphertext << endl << endl;
+
+    cout << "Cipher Text (hex): ";
+    printHex(ciphertext);
+    cout << endl;
+
+    string deciphertext = processOFB(ciphertext, key, nonce);
+    cout << "Decipher Text: " << deciphertext << endl;
+
+}
